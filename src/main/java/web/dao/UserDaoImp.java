56ab5bcff99dc167fd35b40661418ca48618a9ab
Klimov Eduard <edvlklimov@gmail.com>
2024-06-06 14:14:33 +0300

    task is completed

---
+++ src/main/java/web/dao/UserDaoImp.java
@@ -0,0 +1,51 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public void add(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    @Transactional
+    public void update(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> listUsers() {
+        TypedQuery<User> query = entityManager.createQuery("from User", User.class);
+        return query.getResultList();
+    }
+}
